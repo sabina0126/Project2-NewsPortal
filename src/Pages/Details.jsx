f43cc6acd5de4cfe26f23f15149812f6fd792944
Sabina Thokar <109445726+sabina0126@users.noreply.github.com>
2024-08-15 00:07:18 +0545

    updates details file

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,34 +1,50 @@
-import React, { useState, useEffect }  from 'react'
-import NOIMG from '../assets/No_image_Available.jpg'
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import NOIMG from "../assets/No_image_Available.jpg";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Details() {
-  let { id } = useParams()
-  let [data, setData] = useState([])
+  let { id } = useParams();
+  let [data, setData] = useState([]);
   useEffect(() => {
-    axios.get(`https://newsapi.org/v2/top-headlines?articles/${id}&apiKey=54f410e66c894e3196335978f38332fb`)
-      .then(res => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?articles/${id}&apiKey=54f410e66c894e3196335978f38332fb`
+      )
+      .then((res) => {
         setData(res.data);
       });
-  }, [id])
+  }, [id]);
   return (
     <>
-      <div className="container mx-auto">
-        <div className='border shadow-lg py-3'>
-          <div className=''>
-            <h4 className='text-2xl font-bold p-3'>{data.title}</h4>
+      <section>
+        <div className="my-2 bg-blue-900 h-[5px]"></div>
+      </section>
+      <section className="py-5">
+        <div className="container mx-auto">
+          <div className="border shadow-lg py-3">
+            <div className="">
+              <h4 className="text-2xl font-bold p-3">{data.title}</h4>
+              <img
+                src={data.urlToImage ? data.urlToImage : NOIMG}
+                className="w-[100%]"
+                alt=""
+              />
+            </div>
+            <p>{data.descriiption}</p>
+            <p className="underline">Author: {data.author}</p>
+            <p className="underline font-bold py-3">
+              Published at: {data.publishedAt}
+            </p>
+            <p>{data.content}</p>
+            <button className="bg-blue-500 p-2 border rounded-xl">
+              Read More <i className="bi bi-arrow-right" />
+            </button>
           </div>
-          <img src={data.urlToImage ? data.urlToImage : NOIMG} className='w-[100%]' alt="" />
-          <p>{data.descriiption}</p>
-          <p className='underline'>Author: {data.author}</p>
-          <p className='underline font-bold py-3'>Published at: {data.publishedAt}</p>
-          <p>{data.content}</p>
         </div>
-        <button className='bg-blue-500 p-2 border rounded-xl'>Read More <i className="bi bi-arrow-right" /></button>
-      </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Details
+export default Details;
